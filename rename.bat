661fd14951852c8e82b6b739c12cc36335e62955
Daisy-M-Li <111429422+Daisy-M-Li@users.noreply.github.com>
2023-12-13 18:06:49 +0800

    Update rename.bat

--- rename.bat
+++ rename.bat
@@ -1,14 +1,18 @@
+::rename file name
 @echo off
 title rename file
 echo.
 echo star
 echo.
+::input file type eg=sql
 echo.&set /p file_type= sql:
+::input rename string ed=re
 echo.&set /p add_str= cim:
 setlocal enabledelayedexpansion
 for /f "delims=" %%a in ('dir /a /b *.%file_type%') do (
+:: replace 'a' to 're_a'
 ren "%%~a" "%add_str%_%%a")
 echo.
 echo close
-echo.
+echo.s
 pause
